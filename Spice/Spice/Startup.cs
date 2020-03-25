6285bc43c5f8e4bf02c9ff2d7473a4b8d5da3c39
jonclyde <35006633+jonclyde@users.noreply.github.com>
2020-03-25 22:02:12 +0000

    Add razor runtime compilation

--- Spice/Spice/Startup.cs
+++ Spice/Spice/Startup.cs
@@ -33,7 +33,7 @@ namespace Spice
 			services.AddDefaultIdentity<IdentityUser>()
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
-			services.AddRazorPages();
+			services.AddRazorPages().AddRazorRuntimeCompilation();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
