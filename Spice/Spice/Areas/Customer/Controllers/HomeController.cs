7f885b9d4bc08cc65eef003be309a03858fec006
jonclyde <35006633+jonclyde@users.noreply.github.com>
2020-04-01 07:51:46 +0100

    Complete V157

--- Spice/Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Spice/Areas/Customer/Controllers/HomeController.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Spice.Data;
 using Spice.Models;
 using Spice.Models.ViewModels;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Spice.Controllers
 {
@@ -16,7 +16,7 @@ namespace Spice.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ApplicationDbContext _db;
-		
+
 		public HomeController(ApplicationDbContext db)
 		{
 			_db = db;
@@ -32,6 +32,63 @@ namespace Spice.Controllers
 				Coupon = await _db.Coupon.Where(c => c.isActive == true).ToListAsync()
 			};
 			return View(IndexVM);
+		}
+
+		[Authorize]
+		public async Task<IActionResult> Details(int id)
+		{
+			var menuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == id).FirstOrDefaultAsync();
+
+			ShoppingCart cartObj = new ShoppingCart()
+			{
+				MenuItem = menuItemFromDb,
+				MenuItemId = menuItemFromDb.Id
+			};
+			return View(cartObj);
+		}
+
+		[Authorize]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Details(ShoppingCart CartObject)
+		{
+			CartObject.Id = 0;
+			if(ModelState.IsValid)
+			{
+				var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+				var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+				CartObject.ApplicationUserId = claim.Value;
+
+				ShoppingCart cartFromDb = await _db.ShoppingCart.Where(c => c.ApplicationUserId == CartObject.ApplicationUserId
+												&& c.MenuItemId == CartObject.MenuItemId).FirstOrDefaultAsync();
+
+				if(cartFromDb==null)
+				{
+					await _db.ShoppingCart.AddAsync(CartObject);
+				}
+				else
+				{
+					cartFromDb.Count = cartFromDb.Count + CartObject.Count;
+				}
+				await _db.SaveChangesAsync();
+
+				var count = _db.ShoppingCart.Where(c => c.ApplicationUserId == CartObject.ApplicationUserId).ToList().Count();
+				HttpContext.Session.SetInt32("ssCartCount", count);
+
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				var menuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == CartObject.MenuItemId).FirstOrDefaultAsync();
+
+				ShoppingCart cartObj = new ShoppingCart()
+				{
+					MenuItem = menuItemFromDb,
+					MenuItemId = menuItemFromDb.Id
+				};
+
+				return View(cartObj);
+			}
 		}
 
 		public IActionResult Privacy()
--- Spice/Spice/Data/Migrations/20200401052813_AddShoppingCardtoDb.Designer.cs
+++ Spice/Spice/Data/Migrations/20200401052813_AddShoppingCardtoDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spice.Data;
 
 namespace Spice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200401052813_AddShoppingCardtoDb")]
+    partial class AddShoppingCardtoDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -317,9 +319,6 @@ namespace Spice.Data.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<int>("Count")
-                        .HasColumnType("int");
-
-                    b.Property<int>("MenuItemId")
                         .HasColumnType("int");
 
                     b.HasKey("Id");
