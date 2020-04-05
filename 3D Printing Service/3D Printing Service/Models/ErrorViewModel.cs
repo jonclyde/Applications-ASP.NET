f4f7e6b4f3543e51ad0c245b8b36161cda9b388c
jonclyde <35006633+jonclyde@users.noreply.github.com>
2020-04-05 14:24:54 +0100

    3D Printing Service initial commit

---
+++ 3D Printing Service/3D Printing Service/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3D_Printing_Service.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
