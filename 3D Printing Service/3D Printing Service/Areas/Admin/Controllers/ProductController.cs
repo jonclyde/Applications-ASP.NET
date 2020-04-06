38a4e3708205cd7ef4aee779f737816f55289017
jonclyde <35006633+jonclyde@users.noreply.github.com>
2020-04-06 20:06:32 +0100

    Complete products

--- 3D Printing Service/3D Printing Service/Areas/Admin/Controllers/ProductController.cs
+++ 3D Printing Service/3D Printing Service/Areas/Admin/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using _3D_Printing_Service.Data;
+using _3D_Printing_Service.Models;
 using _3D_Printing_Service.Models.ViewModels;
 using _3D_Printing_Service.Utility;
 using Microsoft.AspNetCore.Hosting;
@@ -89,5 +90,141 @@ namespace _3D_Printing_Service.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+        
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            ProductVM.Product = await _db.Product.Include(m => m.Category).Include(m=>m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+            ProductVM.SubCategory = await _db.SubCategory.Where(s => s.CategoryId == ProductVM.Product.CategoryId).ToListAsync();
+
+            if(ProductVM.Product==null)
+            {
+                return NotFound();
+            }
+            return View(ProductVM);
+
+        }
+
+        //POST - EDIT
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPOST(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            ProductVM.Product.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if(!ModelState.IsValid)
+            {
+                ProductVM.SubCategory = await _db.SubCategory.Where(s => s.CategoryId == ProductVM.Product.CategoryId).ToListAsync();
+                return View(ProductVM);
+            }
+
+            //Work on the image saving section
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var productFromDb = await _db.Product.FindAsync(ProductVM.Product.Id);
+
+            if(files.Count > 0)
+            {
+                //new image has been uploaded
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension_new = Path.GetExtension(files[0].FileName);
+
+                //Delete the original file
+                var imagePath = Path.Combine(webRootPath, productFromDb.Image.TrimStart('\\'));
+
+                if(System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+                //we will upload the new file
+                using (var filesStream = new FileStream(Path.Combine(uploads, ProductVM.Product.Id + extension_new), FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+                productFromDb.Image = @"\images\" + ProductVM.Product.Id + extension_new;
+            }
+
+            productFromDb.Name = ProductVM.Product.Name;
+            productFromDb.Description = ProductVM.Product.Description;
+            productFromDb.Price = ProductVM.Product.Price;
+            productFromDb.CategoryId = ProductVM.Product.CategoryId;
+            productFromDb.SubCategoryId = ProductVM.Product.SubCategoryId;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            ProductVM.Product = await _db.Product.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+
+            if(ProductVM.Product == null)
+            {
+                return NotFound();
+            }
+            return View(ProductVM);
+        }
+
+        //GET DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            ProductVM.Product = await _db.Product.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+
+            if(ProductVM.Product == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductVM);
+        }
+
+        //POST DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            Product product = await _db.Product.FindAsync(id);
+
+            if(product != null)
+            {
+                var imagePath = Path.Combine(webRootPath, product.Image.TrimStart('\\'));
+
+                if(System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Exists(imagePath);
+                }
+
+                _db.Product.Remove(product);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
+        
+
 }
+
