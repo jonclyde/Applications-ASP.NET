bec611012984c2fc6056dc0e9a7505ae967bf224
jonclyde <35006633+jonclyde@users.noreply.github.com>
2020-05-04 21:34:46 +0100

    Complete V24

---
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MoviesAPI.Entities;
+using MoviesAPI.Services;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly IRepository repository;
+
+        public GenresController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        [HttpGet("list")]
+        [HttpGet("/allgenres")]
+        public async Task<ActionResult<List<Genre>>> Get()
+        {
+            return await repository.GetAllGenres();
+        }
+
+        [HttpGet("{Id:int}")]
+        public ActionResult<Genre> Get(int Id, [FromHeader] string param2)
+        {
+            var genre = repository.GetGenreById(Id);
+            if(genre == null)
+            {
+                return NotFound();
+            }
+
+            return genre; 
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genre genre)
+        {
+            return NoContent();
+        }
+        [HttpPut]
+        public ActionResult Put([FromBody] Genre genre)
+        {
+            return NoContent();
+        }
+        
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return NoContent();
+        }
+    }
+}
